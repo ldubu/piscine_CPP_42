e9a54ec7fc404dee263b6d74c57cdc4998d81e76
sam <laura.dubuche@gmail.com>
2023-06-01 13:20:48 +0200

    main function written

--- CPP_00/ex01/main.cpp
+++ CPP_00/ex01/main.cpp
@@ -1,19 +1,69 @@
-// #include "PhoneBook.hpp"
-// #include "Contact.hpp"
+#include "PhoneBook.hpp"
+#include "Contact.hpp"
 #include <iostream>
 #include <string>
 
+std::string	ft_ask_info(std::string Message)
+{
+	std::string info;
+	do
+	{
+		std::cout<<Message;
+		std::cin>>info;
+	} while (!info.empty());
+	return (info);
+}
+
+void	ft_add(PhoneBook Repertoire)
+{
+	std::string prenom;
+	std::string nom;
+	std::string surnom;
+	std::string numero;
+	std::string secret;
+	
+	std::cout<<"Ajout d'un nouveau contact dans votre PhoneBook5 !"<<std::endl;
+	prenom = ft_ask_info("Prenom: ");
+	nom = ft_ask_info("Nom de famille: ");
+	surnom = ft_ask_info("Surnom: ");
+	numero = ft_ask_info("Numero de telephone: "); 
+	secret = ft_ask_info("Plus sombre secret: "); 
+	
+	Repertoire.new_contact(prenom, nom, surnom, numero, secret);
+}
+
+void	ft_search(PhoneBook Repertoire)
+{
+	int	index = -1;
+
+	Repertoire.display_phonebook();
+	do
+	{
+		std::cout<<"Quel contact souhaitez-vous affichez ? (Chiffre entre 0 et 7): ";
+		std::cin>>index;
+	} while (index < 0 && index > 7);
+	Repertoire.display_contact(index);
+}
+
 int	main (void)
 {
-	// std::PhoneBook();
+	PhoneBook Repertoire();
+
 	std::string usr_cmd;
 	std::cout<<"Welcome in your PhoneBook5 !! I'm your assistant for navigating this complexe system."<<std::endl;
 	std::cout<<"You can use three commands :"<<std::endl;
 	std::cout<<"	ADD (to add a new contact to your PhoneBook5)"<<std::endl;
 	std::cout<<"	SEARCH (to display a resume of your contact and display the one of your choice)"<<std::endl;
 	std::cout<<"	EXIT (to exit your PhoneBook5, destroy it and destroy me) (in terrible computer pain) (please don't use it TwT)"<<std::endl;
+	do
+	{
 		std::cout<<"Enter your command: ";
 		std::cin>>usr_cmd;
-		std::cout<<std::endl;
+		if (!usr_cmd.compare("ADD"))
+			ft_add(Repertoire);
+		if (!usr_cmd.compare("SEARCH"))
+			ft_search(Repertoire);
+	} while (usr_cmd.compare("EXIT"));
+	return (0);
 
 }
