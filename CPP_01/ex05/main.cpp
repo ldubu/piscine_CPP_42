c4c81007020b0205764b3bf7abed93b97f6c5e70
sam <laura.dubuche@gmail.com>
2023-06-12 12:55:33 +0200

    ex05

---
+++ CPP_01/ex05/main.cpp
@@ -0,0 +1,13 @@
+#include "Harl.hpp"
+
+int	main(void)
+{
+	Harl	Karen;
+
+	Karen.complain("DEBUG");
+	Karen.complain("INFO");
+	Karen.complain("WARNING");
+	Karen.complain("ERROR");
+
+	return (0);
+}
